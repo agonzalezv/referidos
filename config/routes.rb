8a8d4e85ca1736b4bc45d088ec7bbcd7f95d09ab
agonzalezv <andres.gonzalez.vargas@gmail.com>
2016-08-23 22:28:17 +1200

    User registration working. TODO to validate extra fields and to link it to the referrer tables.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,26 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_for :users,
+             controllers: {
+                 passwords: 'user/passwords',
+                 registrations: 'user/registrations',
+                 sessions: 'user/sessions',
+                 confirmations: 'user/confirmations',
+                 unlocks: 'user/unlocks'
+             },
+             path: 'user',
+             path_names: {
+                 sign_in: 'login',
+                 sign_out: 'logout',
+                 password: 'password',
+                 confirmation: 'confirm',
+                 unlock: 'reset',
+                 registration: 'register',
+                 sign_up: 'signup'
+             }
+
+  root to: 'referrers#index'
+
+  #TODO: Usuarios pueden registrarse automaticamente sin autorizacion del administrador. Arreglar.
+
 end
